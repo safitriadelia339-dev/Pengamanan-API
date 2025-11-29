06db037986b25e90f1d5aee8d70a6659515af8c3
safitriadelia339-dev <safitriadelia339@gmail.com>
2025-11-29 18:58:05 +0700

    Membuat Middleware Autorisasi

---
+++ middleware/auth.js
@@ -0,0 +1,36 @@
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = process.env.JWT_SECRET;
+
+//Middleware Autentikasi 
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ') [1];
+
+    if (token == null) {
+        return res.status(401).json({error: 'Akses ditolak, token tidak ditemukan'});
+    }
+
+    jwt.verify(token, JWT_SECRET, (err, decodedPayload) => {
+        if (err) {
+            return res.status(403).json({error:'Token tidak valid atau kadaluwarsa'});
+        }
+        req.user = decodedPayload.user;
+        next();
+    });
+}
+
+// Middleware Autorisasi 
+function authorizeRole(role) {
+    return (req, res, next) => {
+        if (req.user && req.user.role === role) {
+            next();
+        } else {
+            return res.status(403).json({ error: 'Akses Dilarang: Peran tidak memadai' });
+        }
+    };
+}
+
+module.exports = {
+    authenticateToken,
+    authorizeRole
+};
